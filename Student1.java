d52a6a95ebfbfd1d5816cd06e34cbb614ab15f39
Ronish <ronishpanjwani@gmail.com>
2019-02-15 14:56:49 -0800

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,36 @@
+package Training;
+
+import java.util.Set;
+
+public class Student 
+{
+	int studentid;
+	char studentType;
+	Student()
+	{
+		studentid=17;
+		studentType='F';
+	}
+	
+	public void setstudentid(int id)
+	{
+		this.studentid = id;
+		
+	}
+	public int getstudentid()
+	{
+		return studentid;
+		
+	}
+	public void setstudentType(char studentType)
+	{
+		this.studentType =studentType;
+	}
+	public char getstudentType()
+	{
+		return studentType;
+		
+	}
+	
+
+}
