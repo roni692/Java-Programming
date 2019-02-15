9539e3ef8df1dc860df78d5e24c947b97cf36cd0
Ronish <ronishpanjwani@gmail.com>
2019-02-15 11:32:36 -0800

    Passing the Object of Student class used earlier as the parameter
    Therefore it is calling the constructor of the Student class and retrieving the value from the constructor which is being initialized

--- passByVal.java
+++ passByVal.java
@@ -9,12 +9,17 @@ public class passByVal
 		System.out.println(sId);
 		val.passThevalueMethod(sId);
 		System.out.println("the sId are" + sId);
-		
+		new passByVal().passTheValueMethod1(new Student());
 	}
 	public void passThevalueMethod(int sid)
 	{
 		int sId=10;
 		System.out.println("the sId ate"+ sId);
+	}
+	public void passThevalueMethod1(Student obj)
+	{
+		//int sId=10;
+		System.out.println("the sId are"+ sId);
 	}
 
 }
