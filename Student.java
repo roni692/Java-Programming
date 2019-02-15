9154f43fcfbca0a5204db651c6a2c1cef0ef48f1
Ronish <ronishpanjwani@gmail.com>
2019-02-15 14:55:00 -0800

    Constructor with paramaeter

--- Student.java
+++ Student.java
@@ -1,36 +1,40 @@
-package Training;
-
-import java.util.Set;
+package factory;
 
 public class Student 
 {
-	int studentid;
+	int studentId;
 	char studentType;
+	String firstname;
+	String lastname;
+    String studentName;
 	Student()
 	{
-		studentid=10;
-		studentType='F';
+		studentId =550;
 	}
-	
-	public void setstudentid(int id)
-	{
-		this.studentid = id;
-		
-	}
-	public int getstudentid()
-	{
-		return studentid;
-		
-	}
-	public void setstudentType(char studentType)
-	{
-		this.studentType =studentType;
-	}
-	public char getstudentType()
-	{
-		return studentType;
-		
-	}
-	
 
+	Student(char sType,String firstname,String lastname)
+	{
+		studentType=sType;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.studentName = firstname+lastname;
+		
+	}
+	void displayDetails(Student obj)
+	{   
+		//String studentName = firstname+lastname; 
+		System.out.println("Student details");
+		System.out.println("Student Type "  +studentType);
+		System.out.println("Student firstname "  +obj.firstname);
+		System.out.println("Student lastname "  +obj.lastname);
+		System.out.println("Student first and last name "  +obj.studentName);
+		
+	}
+	public static void main(String[] args)
+	{
+		Student studentOne = new Student('D',"Bony","Thomas");
+		studentOne.displayDetails(studentOne);
+		Student studentTwo = new Student('H',"Denil","Bose");
+		studentTwo.displayDetails(studentTwo);
+	}
 }
